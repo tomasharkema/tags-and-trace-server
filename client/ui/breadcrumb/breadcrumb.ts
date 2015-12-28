c4ff6bcd9e447889db47ec183f99011bb2928380
tomas <tomas@harkema.in>
2015-12-28 16:05:02 +0100

    add breadcrumb model

--- client/ui/breadcrumb/breadcrumb.ts
+++ client/ui/breadcrumb/breadcrumb.ts
@@ -1,14 +1,26 @@
 
+class Breadcrumb {
+    name: string;
+    url: string;
+    active: boolean;
+
+    constructor(name:string, url:string, active:boolean) {
+        this.name = name;
+        this.url = url;
+        this.active = active;
+    }
+}
+
 Template['breadcrumb'].helpers({
     parents: () => {
-
-    }
-});
-
-Template['breadcrumb-element'].helpers({
+        return [
+            new Breadcrumb("Home", "/", false),
+            new Breadcrumb("Home", "/", false)
+        ]
+    },
     attributes: () => {
         return {
-            class: "active"
+            "class": this.active ? "" : "active"
         }
     }
 });
