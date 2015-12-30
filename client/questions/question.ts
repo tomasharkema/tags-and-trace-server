e62877d87cd48d835f19455e719bd1fe33daffc6
tomas <tomas@harkema.in>
2015-12-30 15:17:24 +0100

    added simple question add screen

--- client/questions/question.ts
+++ client/questions/question.ts
@@ -22,3 +22,48 @@ Template['questions'].helpers({
         });
     }
 });
+
+Template['questionEdit'].helpers({
+    question: function() {
+        return Questions.findOne({ _id: FlowRouter.current().params['questionId'] })
+    },
+    workflows: function() {
+        return Workflows.find();
+    },
+    incident: function() {
+        console.log(this);
+        return Incidents.findOne(this.incidentId);
+    },
+    workflowSelected: function() {
+        var workflowId = FlowRouter.current().queryParams["workflow"];
+        var question = Questions.findOne({ _id: FlowRouter.current().params['questionId'] });
+        return this._id === workflowId || question.workflowId === this._id;
+    }
+});
+
+Template['questionEdit'].events({
+    "submit .questionEdit": function(event) {
+        var id = FlowRouter.current().params['questionId'];
+        event.preventDefault();
+
+        var label = event.target.label.value;
+        var workflow = event.target.workflow.value;
+
+        if (id) {
+            Questions.update({
+                _id: id
+            }, {$set: {
+                label: label,
+                workflowId: workflow
+            }});
+
+            FlowRouter.go("/question/" + id);
+        } else {
+            var question = new Question(label, [], workflow);
+            Questions.insert(question, function(err, id) {
+                if (err) return console.error(err);
+                FlowRouter.go("/question/" + id);
+            });
+        }
+    }
+});
