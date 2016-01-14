2f8a3a7474be9a326c7321c05e4e0b5c43e858a1
Tomas Harkema <tomas@harkema.in>
2016-01-14 15:07:40 +0100

    add answer page

--- client/answer/answer.ts
+++ client/answer/answer.ts
@@ -1,13 +1,51 @@
 var getAnswer = function() {
-    return Answers.findOne(FlowRouter.current().params['answerId']);
+    let answer = Answers.findOne(FlowRouter.current().params['answerId']);
+    return cast<Answer>(answer, Answer);
 };
 
+var getTagRecord = function() {
+    return TagRecords.findOne({answers: FlowRouter.current().params['answerId']});
+};
+
+this.map = null;
+GoogleMaps.ready('answerLocationMap', function(map) {
+    this.map = map;
+});
+
+Template['answer'].onCreated(function() {
+    this.autorun(function() {
+        console.log("AUTORUN", getTagRecord());
+        GoogleMaps.ready('answerLocationMap', function(map) {
+            console.log("loaded AUTORUN", getTagRecord());
+            let tagrecord = getTagRecord();
+            let marker = new google.maps.Marker({
+                position: new google.maps.LatLng(tagrecord.latitude, tagrecord.longitude),
+                map: this.map.instance,
+                title: 'Hello World!',
+                draggable: true
+            });
+        });
+    });
+});
+
 Template['answer'].helpers({
-    "answer": function() {
-        return getAnswer();
+    "answer": getAnswer,
+    "value": function() {
+        return getAnswer().getValue()
     },
-    "tagrecord": function() {
+    answerMapOptions: function() {
+        if (GoogleMaps.loaded()) {
+            let tagrecord = getTagRecord();
+            return {
+                mapTypeId: google.maps.MapTypeId.SATELLITE,
+                center: new google.maps.LatLng(tagrecord.latitude, tagrecord.longitude),
+                zoom: 17
+            };
+        }
+    },
+    "tagrecord": getTagRecord,
+    question: function() {
         let answer = getAnswer();
-        return null//TagRecords.findOne({answers: {$in: answer._id}})
+        return Questions.findOne(answer.questionId);
     }
 });
